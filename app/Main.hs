02abb4e2caebcaa21beac590e56bd0098a0d8e5f
Jake Wood <jzwood14@yahoo.com>
2022-12-01 23:16:56 -0500

    initial commit

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-
 import Network.Socket
     ( SocketType(..)
     , Family(..)
@@ -16,16 +15,15 @@ import Network.Socket
     )
 
 
-
 main :: IO ()
 main = do
     -- You can use print statements as follows for debugging, they'll be visible when running tests.
     putStrLn "Logs from your program will appear here"
 
     -- Uncomment this to pass stage 1
-    -- sock <- socket AF_INET Stream defaultProtocol
-    -- setSocketOption sock ReuseAddr 1
-    -- bind sock (SockAddrInet 6379 0)
-    -- listen sock 5
-    -- _ <- accept sock
-    -- return ()
+    sock <- socket AF_INET Stream defaultProtocol
+    setSocketOption sock ReuseAddr 1
+    bind sock (SockAddrInet 6379 0)
+    listen sock 5
+    _ <- accept sock
+    return ()
