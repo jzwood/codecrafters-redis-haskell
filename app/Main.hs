6f201d755ff837de0d1ed35edd531e6b50838ea5
Jake Wood <jzwood14@yahoo.com>
2022-12-15 22:30:06 -0500

    event loop seems to work

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Control.Concurrent
+  ( forkIO
+  , ThreadId
+  )
 import Network.Socket
     ( SocketType(..)
+    , ShutdownCmd(..)
     , Family(..)
     , SocketOption(..)
     , SockAddr(..)
     , Socket
+    , shutdown
     , socket
     , listen
     , bind
@@ -17,11 +23,25 @@ import Network.Socket
     )
 import Network.Socket.ByteString
 
-respond :: Socket -> IO ()
-respond conn = do
+handle :: Socket -> IO ()
+handle conn = do
     resp <- recv conn 1024
-    send conn "+PONG\r\n"
-    respond conn
+    if resp == ""
+        then
+            close conn
+        else do
+            print ("A", resp)
+            send conn "+PONG\r\n"
+            shutdown conn ShutdownBoth
+            _ <- forkIO $ handle conn
+            return ()
+
+loop :: Socket -> IO ()
+loop sock = do
+  (conn, address) <- accept sock
+  print ("B", conn, address)
+  handle conn
+  loop sock
 
 main :: IO ()
 main = do
@@ -31,7 +51,6 @@ main = do
     setSocketOption sock ReuseAddr 1
     bind sock (SockAddrInet 6379 0)
     listen sock 5
-    (conn, address) <- accept sock
-    respond conn
+    loop sock
     print "BYEEEE"
     return ()
